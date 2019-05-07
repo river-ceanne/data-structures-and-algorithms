4a741c037029e4245cb878d18ff1a905fc2d2cfd
river-ceanne <snvencer@gmail.com>
2019-05-07 13:31:36 -0700

    DONEEEEE

--- code-challenges/2d-arrays/challenges-12.test.js
+++ code-challenges/2d-arrays/challenges-12.test.js
@@ -210,6 +210,17 @@ For example, excel('1,1,1\n4,4,4\n9,9,9') returns [3, 12, 27].
 
 const excel = (str) => {
   // Solution code here...
+  let arr = str.split('\n');
+
+  arr = arr.map(val => val.split(',')).map(val => {
+    let sum = 0;
+    val.forEach(each => {
+      sum += parseInt(each);
+    });
+    return sum;
+  });
+  
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
