a1894072608e91866c94788133c67c3e3741fb7e
river-ceanne <snvencer@gmail.com>
2019-05-29 11:16:55 -0700

    binary search code done
    Signed-off-by: river-ceanne <snvencer@gmail.com>

---
+++ code-401-challenges/src/main/java/code/challenges/BinarySearch.java
@@ -0,0 +1,28 @@
+package code.challenges;
+
+public class BinarySearch {
+
+    // Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key.
+    // Without utilizing any of the built-in methods available to your language, return the index of the array’s
+    // element that is equal to the search key, or -1 if the element does not exist.
+
+    public static int binarySearch(int[] arr, int key){
+        int max = arr.length - 1;
+        int min = 0;
+        int mid;
+
+        while(min < max){
+            mid = min + (max - min) / 2;
+
+            if(arr[mid] == key){
+                return mid;
+            }else if(key < arr[mid]){
+                max = mid - 1;
+            }else {
+                min = mid + 1;
+            }
+
+        }
+        return -1;
+    }
+}
