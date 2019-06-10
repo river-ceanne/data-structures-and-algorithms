d502d0f4f09f1d7d2d822f366731172b83279c36
river-ceanne <snvencer@gmail.com>
2019-06-10 16:31:11 -0700

    initial code challenge set-up QueuesWithStacks

---
+++ Data-Structures/src/main/java/queuewithstacks/PseudoQueue.java
@@ -0,0 +1,21 @@
+package queuewithstacks;
+
+import stacksandqueues.Stack;
+
+public class PseudoQueue {
+
+    protected Stack firstStack;
+    protected Stack secondStack;
+
+    public PseudoQueue(){}
+
+    public void enqueue(String val){
+
+    }
+
+    public String dequeue(){
+        
+        return "";
+    }
+
+}
