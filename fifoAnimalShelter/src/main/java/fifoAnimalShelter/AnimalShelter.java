b9be5d1a6ed8ec4986a6b5211ecbd367324dfff4
river-ceanne <snvencer@gmail.com>
2019-06-12 09:57:18 -0700

    Animal Shelter Class made with needed funcs

--- fifoAnimalShelter/src/main/java/fifoAnimalShelter/AnimalShelter.java
+++ fifoAnimalShelter/src/main/java/fifoAnimalShelter/AnimalShelter.java
@@ -1,7 +1,39 @@
 package fifoAnimalShelter;
 
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class AnimalShelter {
+
+    protected ArrayList<Animal> animals;
+
+    public AnimalShelter(){
+        animals = new ArrayList<>();
+    }
+
+    public void enqueue(Animal animal){
+        animals.add(animal);
+    }
+
+    public Animal dequeue(String pref){
+        pref = pref.toLowerCase();
+        int decrementor = 1;
+        Animal animal;
+        switch (pref){
+            case "dog":
+                do{
+                    animal = animals.get(animals.size() - decrementor);
+                }while(animal.getClass().getTypeName() != "fifoAnimalShelter.Dog");
+                return animal;
+            case "cat":
+                do{
+                    animal = animals.get(animals.size() - decrementor);
+                }while(animal.getClass().getTypeName() != "fifoAnimalShelter.Cat");
+                return animal;
+            default:
+                return null;
+        }
+
+    }
 
 }
