d385935b995538da5dba65f23dbbfd8b8f197df6
river-ceanne <snvencer@gmail.com>
2019-06-12 14:59:22 -0700

    multibracketValidation function made with some tests

--- src/main/java/multibracketvalidation/MultiBracketValidation.java
+++ src/main/java/multibracketvalidation/MultiBracketValidation.java
@@ -3,6 +3,12 @@
  */
 package multibracketvalidation;
 
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class MultiBracketValidation {
 
     public static void main(String[] args) {
@@ -10,6 +16,40 @@ public class MultiBracketValidation {
     }
 
     public static boolean multiBracketValidation(String input){
+
+        Stack<String> openBrackStack = new Stack<>();
+
+        HashMap<String,String> bracketPairs = new HashMap<>();
+        bracketPairs.put("{","}");
+        bracketPairs.put("(",")");
+        bracketPairs.put("[","]");
+
+        String[] inputSplit = input.split("");
+        for (String var : inputSplit) {
+
+            if(bracketPairs.containsKey(var)) {
+                openBrackStack.push(var);
+                continue;
+            }
+
+            if(bracketPairs.containsValue(var)){
+
+                if(openBrackStack.empty()) return false;
+
+                String topOpenBracket = openBrackStack.pop();
+
+                for (Map.Entry<String, String> entry : bracketPairs.entrySet()) {
+                    String k = entry.getKey();
+                    String v = entry.getValue();
+                    if (var == v && topOpenBracket != k) {
+                        return false;
+                    }
+
+                }//end of hashmap for loop
+            }
+
+        }//end of string split for loop
+
 
         return true;
     }
--- src/test/java/multibracketvalidation/MultiBracketValidationTest.java
+++ src/test/java/multibracketvalidation/MultiBracketValidationTest.java
@@ -4,10 +4,43 @@
 package multibracketvalidation;
 
 import org.junit.Test;
+
+import static multibracketvalidation.MultiBracketValidation.multiBracketValidation;
 import static org.junit.Assert.*;
 
 public class MultiBracketValidationTest {
-    @Test public void testAppHasAGreeting() {
+    @Test
+    public void testMultiBracketValidationOne() {
+        assertFalse(multiBracketValidation("{{}]]"));
+    }
 
+    @Test
+    public void testMultiBracketValidationTwo() {
+        assertFalse(multiBracketValidation("]]]]"));
+    }
+
+    @Test
+    public void testMultiBracketValidationThree() {
+        assertTrue(multiBracketValidation("{}{}{}"));
+    }
+
+    @Test
+    public void testMultiBracketValidationFour() {
+        assertTrue(multiBracketValidation("[{}{}()]"));
+    }
+
+    @Test
+    public void testMultiBracketValidationFiveWorded() {
+        assertTrue(multiBracketValidation("[{Bubbles}Orchids(2)]"));
+    }
+
+    @Test
+    public void testMultiBracketValidationSixWorded() {
+        assertFalse(multiBracketValidation("[{Bubbles}Orchids(2)]{"));
+    }
+
+    @Test
+    public void testMultiBracketValidationSeven() {
+        assertTrue(multiBracketValidation("[({([])})]"));
     }
 }
