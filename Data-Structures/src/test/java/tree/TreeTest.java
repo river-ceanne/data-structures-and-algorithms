cb5f670a28fe536ed4e90f6c6fc3bc295f49eed0
river-ceanne <snvencer@gmail.com>
2019-06-17 10:13:55 -0700

    last edits and cleaning

--- Data-Structures/src/test/java/tree/TreeTest.java
+++ Data-Structures/src/test/java/tree/TreeTest.java
@@ -8,13 +8,6 @@ import static org.junit.Assert.*;
 
 public class TreeTest {
 
-    // Can successfully instantiate an empty tree
-    //Can successfully instantiate a tree with a single root node
-    //Can successfully add a left child and right child to a single root node
-    //Can successfully return a collection from a preorder traversal
-    //Can successfully return a collection from an inorder traversal
-    //Can successfully return a collection from a postorder traversal
-
     /////----- BINARY TREE NODE TEST -------/////////
 
     @Test
