b05bebec5cf7d487282d99889fc7ab737d562ef0
river-ceanne <snvencer@gmail.com>
2019-06-16 19:26:25 -0700

    Binary Tree Node class made

---
+++ Data-Structures/src/main/java/tree/BinaryTreeNode.java
@@ -0,0 +1,41 @@
+package tree;
+
+public class BinaryTreeNode {
+
+    private int value;
+    private BinaryTreeNode leftChild;
+    private BinaryTreeNode rightChild;
+
+    public BinaryTreeNode(){}
+
+    public BinaryTreeNode(int value, BinaryTreeNode leftChild, BinaryTreeNode rightChild){
+        this.value = value;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public BinaryTreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(BinaryTreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public BinaryTreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(BinaryTreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+}
