015c68feaa911af679d39a0d68c8445c4fb0a82a
river-ceanne <snvencer@gmail.com>
2019-07-09 10:12:04 -0700

    functions - feature tasks made

--- Data-Structures/src/main/java/Data/Structures/Hashtable.java
+++ Data-Structures/src/main/java/Data/Structures/Hashtable.java
@@ -18,13 +18,13 @@ public class Hashtable {
         return hashValue;
     }
 
-    public void set(String key, String value) {
+    public void add(String key, String value) {
         int hashkey = hash(key);
 
         if (this.map[hashkey] == null) {
             map[hashkey] = new Node(key,value);
         }
-        else {
+        else {//collision handling
             Node temp = map[hashkey];
             map[hashkey] = new Node(key,value);
             map[hashkey].setNextNode(temp);
@@ -35,9 +35,21 @@ public class Hashtable {
 
 //    Implement a Hashtable with the following methods:
 //
-//add: takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+//add: takes in both the key and value. This method should hash the key,
+// and add the key and value pair to the table, handling collisions as needed.
 //get: takes in the key and returns the value from the table.
 //contains: takes in the key and returns a boolean, indicating if the key exists in the table already.
 //hash: takes in an arbitrary key and returns an index in the collection.
+
+    public String get(String key){
+        int hashkey = hash(key);
+        return this.map[hashkey].getValue();
+    }
+
+    public boolean contains(String key){
+        int hashkey = hash(key);
+        return this.map[hashkey] != null;
+
+    }
 
 }
