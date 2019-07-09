7d9376faf330c29454115c928f4b4487bf6c8263
river-ceanne <snvencer@gmail.com>
2019-07-09 10:47:08 -0700

    clean up and add resource

--- Data-Structures/src/main/java/Data/Structures/Hashtable.java
+++ Data-Structures/src/main/java/Data/Structures/Hashtable.java
@@ -1,5 +1,7 @@
 package Data.Structures;
 
+//resources: hashing function/base code - John Cokos
+
 public class Hashtable {
 
     protected Node[] map;
@@ -32,14 +34,6 @@ public class Hashtable {
 
 
     }
-
-//    Implement a Hashtable with the following methods:
-//
-//add: takes in both the key and value. This method should hash the key,
-// and add the key and value pair to the table, handling collisions as needed.
-//get: takes in the key and returns the value from the table.
-//contains: takes in the key and returns a boolean, indicating if the key exists in the table already.
-//hash: takes in an arbitrary key and returns an index in the collection.
 
     public String get(String key){
         int hashkey = hash(key);
