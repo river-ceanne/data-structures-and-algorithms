15f008e74b903133af73ecfbee94841ff1dab9e4
river-ceanne <snvencer@gmail.com>
2019-07-10 14:49:47 -0700

    3 tests made

--- code-401-challenges/src/test/java/code/challenges/RepeatedWordTest.java
+++ code-401-challenges/src/test/java/code/challenges/RepeatedWordTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class RepeatedWordTest {
 
     @Test
-    public void testInsertionTestReverse(){
+    public void testRepeatedWord1(){
 
         assertEquals("it", RepeatedWord.repeatedWord("It was the best of times, it was the worst of times, " +
                 "it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch " +
@@ -17,6 +17,21 @@ public class RepeatedWordTest {
                 "direct to Heaven, we were all going direct the other way – in short, the period was so far like the present " +
                 "period, that some of its noisiest authorities insisted on its being received, for good or for evil, " +
                 "in the superlative degree of comparison only..."));
+
+    }
+
+    @Test
+    public void testRepeatedWord2(){
+
+        assertEquals("book", RepeatedWord.repeatedWord("a book is the book i like to read in a sunny day"));
+
+    }
+
+    @Test
+    public void testRepeatedWord3(){
+
+        assertEquals("summer", RepeatedWord.repeatedWord("It was a queer, sultry summer , the " +
+                "summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."));
 
     }
 
