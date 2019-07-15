5774408f17a99866f9ad6cdc497015c753b8adfe
river-ceanne <snvencer@gmail.com>
2019-07-15 11:59:59 -0700

    basic structure of graph files added

--- Data-Structures/src/main/java/Data/Structures/Graph.java
+++ Data-Structures/src/main/java/Data/Structures/Graph.java
@@ -1,4 +1,72 @@
 package Data.Structures;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class Graph {
+
+    private int numVertices;
+    private LinkedList<Integer>[] adjList;
+
+    public Graph(){}
+
+
+    public int getNumVertices() {
+        return numVertices;
+    }
+
+    public void setNumVertices(int numVertices) {
+        this.numVertices = numVertices;
+    }
+
+    public LinkedList<Integer>[] getAdjList() {
+        return adjList;
+    }
+
+    public void setAdjList(LinkedList<Integer>[] adjList) {
+        this.adjList = adjList;
+    }
+
+
+//AddNode()
+//Adds a new node to the graph
+//Takes in the value of that node
+//Returns the added node
+
+//AddEdge()
+//Adds a new edge between two nodes in the graph
+//Include the ability to have a “weight”
+//Takes in the two nodes to be connected by the edge
+//Both nodes should already be in the Graph
+
+//GetNodes()
+//Returns all of the nodes in the graph as a collection (set, list, or similar)
+
+//GetNeighbors()
+//Returns a collection of nodes connected to the given node
+//Takes in a given node
+//Include the weight of the connection in the returned collection
+
+//Size()
+//Returns the total number of nodes in the graph
+
+    public Node addNode(){
+        return new Node();
+    }
+
+    public void addEdge(){
+
+    }
+
+    public HashSet<Node> getNodes(){
+        return new HashSet<>();
+    }
+
+    public HashSet<Node> getNeighbors(Node node){
+        return new HashSet<>();
+    }
+
+    public int size(){
+        return numVertices;
+    }
 }
--- Data-Structures/src/test/java/Data/Structures/GraphTest.java
+++ Data-Structures/src/test/java/Data/Structures/GraphTest.java
@@ -1,4 +1,14 @@
 package Data.Structures;
 
 public class GraphTest {
+
+//  Node can be successfully added to the graph
+//  An edge can be successfully added to the graph
+//  A collection of all nodes can be properly retrieved from the graph
+//  All appropriate neighbors can be retrieved from the graph
+//  Neighbors are returned with the weight between nodes included
+//  The proper size is returned, representing the number of nodes in the graph
+//  A graph with only one node and edge can be properly returned
+//  An empty graph properly returns null
+
 }
