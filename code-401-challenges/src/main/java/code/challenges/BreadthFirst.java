2a34875f5b145b330341d15edd229cb2eb5d7809
river-ceanne <snvencer@gmail.com>
2019-07-16 13:22:55 -0700

    function breadth first done and tests made

--- code-401-challenges/src/main/java/code/challenges/BreadthFirst.java
+++ code-401-challenges/src/main/java/code/challenges/BreadthFirst.java
@@ -3,17 +3,35 @@ package code.challenges;
 import graph.Node;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BreadthFirst {
 //    Extend your graph object with a breadth-first traversal method
 //    that accepts a starting node. Without utilizing any of the built-in methods available
 //    to your language, return a collection of nodes in the order they were visited. Display the collection.
 
-    public static HashSet<Node> breadthFirst(Node node){
+    public static HashSet<Node<String>> breadthFirst(Node<String> node){
 
-        HashSet<Node> hashSet = new HashSet<>();
+        Queue<Node<String>> queue = new LinkedList<>();
+        HashSet<Node<String>> visited = new HashSet<>();
 
-        return hashSet;
+        queue.add(node);
+
+        while(!queue.isEmpty()){
+
+            Node<String> current = queue.remove();
+
+            if(visited.contains(current)) continue;
+
+            visited.add(current);
+
+            for(Node<String> neighbor: current.getNeighbors()){
+                queue.add(neighbor);
+            }
+        }
+
+        return visited;
 
     }
 
--- code-401-challenges/src/test/java/code/challenges/BreadthFirstTest.java
+++ code-401-challenges/src/test/java/code/challenges/BreadthFirstTest.java
@@ -1,36 +1,91 @@
 package code.challenges;
 
 import graph.Graph;
+import graph.Node;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class BreadthFirstTest {
 
-
     @Test
-    public void testGraphEmpty(){
-        Graph graph = new Graph();
-        assertNull(graph.getNodes());
+    public void testBreadthFirstGetName(){
+        Node<String> node = new Node<>("node");
+        HashSet<Node<String>> res = BreadthFirst.breadthFirst(node);
+
+        assertEquals("node",res.iterator().next().getName());
+
     }
 
-    private Graph initialize(){
-        Graph graph = new Graph(5);
-        graph.addNode(1);
-        graph.addNode(5);
-        graph.addNode(2);
-        graph.addNode(4);
-        graph.addNode(3);
+    @Test
+    public void testBreadthFirstOneNode(){
+        Node<String> node = new Node<>("only one");
+        HashSet<Node<String>> res = BreadthFirst.breadthFirst(node);
 
-        graph.addEdge(1,2,2);
-        graph.addEdge(2,3,7);
+        HashSet<Node<String>> exp = new HashSet<>();
+        exp.add(node);
+        assertEquals(exp,res);
 
-        graph.addEdge(3,4,2);
-        graph.addEdge(4,5,7);
+    }
 
-        graph.addEdge(5,1,7);
+    @Test
+    public void testBreadthFirst(){
+        HashSet<Node<String>> res = BreadthFirst.breadthFirst(initialize());
 
-        return graph;
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        for(Node<String> node: res){
+            System.out.println(node.getName());
+        }
+
+
+        assertEquals("1\n" +
+                "2\n" +
+                "5\n" +
+                "4\n" +
+                "3\n", outContent.toString());
+    }
+
+    private Node<String> initialize(){
+        Node<String> node1 = new Node<>("1");
+        Node<String> node2 = new Node<>("2");
+        Node<String> node3 = new Node<>("3");
+        Node<String> node4 = new Node<>("4");
+        Node<String> node5 = new Node<>("5");
+
+        ArrayList<Node<String>> neighbor1 = new ArrayList<>();
+        neighbor1.add(node2);
+        neighbor1.add(node5);
+        node1.setNeighbors(neighbor1);
+
+        ArrayList<Node<String>> neighbor2 = new ArrayList<>();
+        neighbor2.add(node1);
+        neighbor2.add(node3);
+        node2.setNeighbors(neighbor2);
+
+        ArrayList<Node<String>> neighbor3 = new ArrayList<>();
+        neighbor3.add(node2);
+        neighbor3.add(node4);
+        node3.setNeighbors(neighbor3);
+
+        ArrayList<Node<String>> neighbor4 = new ArrayList<>();
+        neighbor4.add(node3);
+        neighbor4.add(node5);
+        node4.setNeighbors(neighbor4);
+
+        ArrayList<Node<String>> neighbor5 = new ArrayList<>();
+        neighbor5.add(node1);
+        node5.setNeighbors(neighbor5);
+
+
+        return node1;
     }
 
 
